56017e4beb55504cb41c124aaa3c052ee585c2e7
yoongun97 <tesung5470@naver.com>
2024-03-21 16:55:39 +0900

    promise -> async await으로 수정

--- examples/nextjs/src/components/Demo.tsx
+++ examples/nextjs/src/components/Demo.tsx
@@ -1,6 +1,6 @@
 import { useMicVAD, utils } from "@ricky0123/vad-react"
 import { useState } from "react"
-import axios from 'axios'
+import axios from "axios"
 
 const OPENAI_API_KEY = process.env.NEXT_PUBLIC_OPENAI_API_KEY
 
@@ -11,30 +11,46 @@ export const Demo = () => {
       const wavBuffer = utils.encodeWAV(audio)
       const base64 = utils.arrayBufferToBase64(wavBuffer)
       const url = `data:audio/wav;base64,${base64}`
-      fetch(url).then((res) => {
-        res.blob().then((blob) => {
-          const formData = new FormData();
-          formData.append("file", blob, "audio.wav");
-          formData.append("model", "whisper-1");
-          axios.post('https://api.openai.com/v1/audio/transcriptions', formData, {
-            headers: {
-              Authorization: `Bearer ${OPENAI_API_KEY}`,
-              'Content-Type': 'multipart/form-data'
-            },
-        }).then((res) => {
-          console.log(res.data.text);
-          const newText = res.data.text
-          setAudioList((old) => {
-            return [newText, ...old]
-          })
-        })
+      urlToFile(url)
+    },
+  })
+
+  const urlToFile = async (url: string) => {
+    try {
+      const res = await fetch(url)
+      const blob = await res.blob()
+      fileToText(blob)
+    } catch (err) {
+      console.error(err)
+    }
+  }
+
+  const fileToText = async (blob: Blob) => {
+    const formData = new FormData()
+    formData.append("file", blob, "audio.wav")
+    formData.append("model", "whisper-1")
+    try {
+      const res = await axios.post(
+        "https://api.openai.com/v1/audio/transcriptions",
+        formData,
+        {
+          headers: {
+            Authorization: `Bearer ${OPENAI_API_KEY}`,
+            "Content-Type": "multipart/form-data",
+          },
+        },
+      )
+      const newText = res.data.text
+      setAudioList((old) => {
+        return [newText, ...old]
       })
-    })
-  },
-})
+    } catch (err) {
+      console.error(err)
+    }
+  }
 
   return (
-    <div className='mx-auto flex flex-col items-center pt-6 '>
+    <div className="mx-auto flex flex-col items-center pt-6 ">
       <h6>State</h6>
       <p>{vad.listening && "듣는 중"}</p>
       <p>{vad.loading && "대기 중"}</p>
@@ -46,18 +62,12 @@ export const Demo = () => {
       {/* <button onClick={vad.toggle}>toggle</button> */}
       <h6>audioList</h6>
       <ol>
-        {audioList.map((val, index)=>{
-          return (
-            <li key={index}>
-            {val}
-            </li>
-          )
+        {audioList.map((val, index) => {
+          return <li key={index}>{val}</li>
         })}
       </ol>
-      
     </div>
   )
 }
 
 export default Demo
-
