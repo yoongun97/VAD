4a8afb298a113da7f3583151bf446834c07c88dd
yoongun97 <tesung5470@naver.com>
2024-03-21 11:35:18 +0900

    vad sample clone coding

--- examples/nextjs/src/components/Demo.tsx
+++ examples/nextjs/src/components/Demo.tsx
@@ -13,24 +13,43 @@ export const Demo = () => {
       })
     },
   })
+
+
   return (
-    <div>
-      <h6>Listening</h6>
-      {!vad.listening && "Not"} listening
+    <div className='mx-auto flex flex-col items-center pt-6 '>
+      {/* <h6>Listening</h6>
+      <p>{!vad.listening && "Not"} listening</p>
       <h6>Loading</h6>
-      {!vad.loading && "Not"} loading
+      <p>{!vad.loading && "Not"} loading</p>
       <h6>Errored</h6>
-      {!vad.errored && "Not"} errored
+      <p>{!vad.errored && "Not"} errored</p>
       <h6>User Speaking</h6>
-      {!vad.userSpeaking && "Not"} speaking
+      <p> {!vad.userSpeaking && "Not"} speaking</p>
       <h6>Audio count</h6>
-      {audioList.length}
+      <p>{audioList.length}</p> */}
+      <h6>State</h6>
+      <p>{vad.listening && "듣는 중"}</p>
+      <p>{vad.loading && "대기 중"}</p>
+      <p>{vad.errored && "errored"}</p>
+      <p>{vad.userSpeaking && "말하는 중"}</p>
       <h6>Start/Pause</h6>
-      <button onClick={vad.pause}>Pause</button>
-      <button onClick={vad.start}>Start</button>
-      <button onClick={vad.toggle}>Toggle</button>
+      {/* <button onClick={vad.pause}>Pause</button>
+      <button onClick={vad.start}>Start</button> */}
+      <button onClick={vad.toggle}>toggle</button>
+      <h6>audioList</h6>
+      <ol>
+        {audioList.map((val)=>{
+          return (
+            <li>
+            <audio src={val} controls/>
+            </li>
+          )
+        })}
+      </ol>
+      
     </div>
   )
 }
 
 export default Demo
+
